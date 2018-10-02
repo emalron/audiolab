64244bb2c3d0ef9bc131ebbb63bd7100bbd9ac77
Emalron <pjm2108@naver.com>
2018-10-02 15:03:09 +0900

    feature/issue29-2

--- js/gameState.js
+++ js/gameState.js
@@ -206,9 +206,9 @@ function getInput(key) {
                     
                     g.girl.position = dest;
                 }, this);
-                ga('send', 'event', 'catch', 'click', 'catch');
+                ga('send', 'event', 'spcae', 'catch',g.boy.x + ','+g.boy.y);
             }
-            ga('send', 'event', 'catch', 'click', 'miss');
+            ga('send', 'event', 'space', 'miss', g.boy.x + ','+g.boy.y);
         }
     }
 }
